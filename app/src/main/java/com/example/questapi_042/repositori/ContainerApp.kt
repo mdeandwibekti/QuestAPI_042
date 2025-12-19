4442439ee440f8d3a3eed0612e884d33e8a10e12
mdeandwibekti <mdeandwibekti@gmail.com>
2025-12-19 20:35:17 +0700

    membuat val RepositoryDataSiswa.kt

---
+++ app/src/main/java/com/example/questapi_042/repositori/ContainerApp.kt
@@ -0,0 +1,15 @@
+package com.example.questapi_042.repositori
+
+import android.app.Application
+import com.example.questapi_042.apiservice.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+interface ContainerApp {
+    val repositoryDataSiswa: RepositoryDataSiswa
+}
+
