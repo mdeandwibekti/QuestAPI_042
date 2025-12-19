342076f2ce2f99308663296e685cfc02d04e3942
mdeandwibekti <mdeandwibekti@gmail.com>
2025-12-19 21:00:09 +0700

    membuat function HostNavigasi agar navigasi start dari homescreen

--- app/src/main/java/com/example/questapi_042/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_042/uicontroller/PetaNavigasi.kt
@@ -17,3 +17,22 @@ import com.example.questapi_082.view.HomeScreen
 fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
     HostNavigasi(navController = navController)
 }
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+//                    navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+    }
+}
