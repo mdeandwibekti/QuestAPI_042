62c556ad0a92f61d5bdc550f50f2e6ff15c1fc6a
mdeandwibekti <mdeandwibekti@gmail.com>
2025-12-20 07:31:21 +0700

    membuat penyediaviewmodel

---
+++ app/src/main/java/com/example/questapi_042/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.questapi_042.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questapi_042.repositori.AplikasiDataSiswa
+import com.example.questapi_042.viewmodel.EntryViewModel
+import com.example.questapi_042.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+                AplikasiDataSiswa
+        )
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container
+                    .repositoryDataSiswa
+            )
+        }
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container
+                    .repositoryDataSiswa
+            )
+        }
+    }
+}
