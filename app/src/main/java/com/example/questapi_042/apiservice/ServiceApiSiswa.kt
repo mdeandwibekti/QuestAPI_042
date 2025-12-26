958cfedb5a5fbeac9670538cfac4f5c5f136d59d
mdeandwibekti <mdeandwibekti@gmail.com>
2025-12-26 21:01:48 +0700

    menambahkan dan mengonfigurasi endpoint API Retrofit untuk melakukan operasi CRUD (Read, Update, Delete)

--- app/src/main/java/com/example/questapi_042/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_042/apiservice/ServiceApiSiswa.kt
@@ -9,10 +9,20 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Query
 
+
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id : Int): DataSiswa
+
+    @PUT("editTM.php")
+    suspend fun editSatuSiswa(@Query("id")id: Int, @Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    @DELETE("DeleteTM.php")
+    suspend fun hapusSatuSiswa(@Query("id")id:Int): retrofit2.Response<Void>
 }
