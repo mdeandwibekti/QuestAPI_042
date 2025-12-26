6806510e50dbf77ea2dc73f24baafb273e2a4bcb
mdeandwibekti <mdeandwibekti@gmail.com>
2025-12-26 21:08:03 +0700

    menambahkan destinasi navigasi untuk halaman edit data siswa

---
+++ app/src/main/java/com/example/questapi_042/uicontroller/route/DestinasiEdit.kt
@@ -0,0 +1,12 @@
+package com.example.questapi_042.uicontroller.route
+
+import com.example.questapi_042.R
+
+object DestinasiEdit : DestinasiNavigasi {
+
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
