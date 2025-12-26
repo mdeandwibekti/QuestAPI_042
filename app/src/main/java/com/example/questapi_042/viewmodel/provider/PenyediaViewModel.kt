d63dac49d43db01efeb72cde247da07ae42ab5b0
mdeandwibekti <mdeandwibekti@gmail.com>
2025-12-26 21:12:52 +0700

    menambahkan inisialisasi DetailViewModel pada ViewModelFactory dengan memanfaatkan SavedStateHandle dan Repository Data Siswa.

--- app/src/main/java/com/example/questapi_042/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_042/viewmodel/provider/PenyediaViewModel.kt
@@ -6,27 +6,28 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi_042.repositori.AplikasiDataSiswa
+import com.example.questapi_042.viewmodel.DetailViewModel
+import com.example.questapi_042.viewmodel.EditViewModel
 import com.example.questapi_042.viewmodel.EntryViewModel
 import com.example.questapi_042.viewmodel.HomeViewModel
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+fun CreationExtras.aplikasiDataSiswa():AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
                 AplikasiDataSiswa
         )
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container
+            .repositoriDataSiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container
+            .repositoriDataSiswa) }
         initializer {
-            HomeViewModel(
-                aplikasiDataSiswa().container
-                    .repositoryDataSiswa
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoriDataSiswa
             )
         }
-        initializer {
-            EntryViewModel(
-                aplikasiDataSiswa().container
-                    .repositoryDataSiswa
-            )
-        }
+
     }
 }
