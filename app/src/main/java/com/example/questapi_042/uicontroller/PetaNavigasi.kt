c4e5ef26c0cf1cfcdb8e2bcac79c78851f012469
mdeandwibekti <mdeandwibekti@gmail.com>
2025-12-26 21:11:48 +0700

    menambahkan konfigurasi NavHost composable untuk halaman edit siswa dengan dukungan argumen navigasi berupa ID siswa, serta aksi navigasi kembali ke halaman utama atau ke layar sebelumnya.

--- app/src/main/java/com/example/questapi_042/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_042/uicontroller/PetaNavigasi.kt
@@ -63,6 +63,16 @@ fun HostNavigasi(
             )
         }
 
-
+        composable(
+            DestinasiEdit.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEdit.itemIdArg) {
+                type = NavType.IntType
+            })
+        ){
+            EditSiswaScreen(
+                navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
     }
 }
