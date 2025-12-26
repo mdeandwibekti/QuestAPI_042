ed80e1ec594c02ef2cffc2d27e352d52dca62aaf
mdeandwibekti <mdeandwibekti@gmail.com>
2025-12-26 21:06:42 +0700

    menambahkan konfigurasi destinasi navigasi untuk halaman detail siswa

---
+++ app/src/main/java/com/example/questapi_042/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,12 @@
+package com.example.questapi_042.uicontroller.route
+
+import com.example.questapi_042.R
+
+object DestinasiDetail : DestinasiNavigasi {
+
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
